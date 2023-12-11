6c32fdfd8451ce823c946c0a4236d64e2783fbc3
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-11 14:47:20 +0000

    refactor: define type for return of function

--- modules/alternate_case.py
+++ modules/alternate_case.py
@@ -1,7 +1,7 @@
-def to_alternating_case(string: str):
+def to_alternating_case(string: str) -> str:
     if not isinstance(string, str):
         raise ValueError("❗️ The input should be a string")
-    result_string = ""
+    result_string: str = ""
     for letter in string:
         if letter.isupper():
             result_string += letter.lower()
