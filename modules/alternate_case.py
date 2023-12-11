32d17aac0812f8bd1762f2a692c8f7b34f0e23fb
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-11 14:47:52 +0000

    refactor: simplify the code with swapcase

--- modules/alternate_case.py
+++ modules/alternate_case.py
@@ -1,10 +1,4 @@
 def to_alternating_case(string: str) -> str:
     if not isinstance(string, str):
         raise ValueError("❗️ The input should be a string")
-    result_string: str = ""
-    for letter in string:
-        if letter.isupper():
-            result_string += letter.lower()
-        else:
-            result_string += letter.upper()
-    return result_string
+    return string.swapcase()
